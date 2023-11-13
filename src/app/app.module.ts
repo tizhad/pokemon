bb93ce032b9ea779ccbf9c7c5a4e009d9364a382
tina <tinarezanezhaad@gmail.com>
2023-11-13 17:08:34 +0100

    Css added to the style.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { ItemListComponent } from './item-list/item-list.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponent,
+    HeaderComponent,
+    FooterComponent,
+    ItemListComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule

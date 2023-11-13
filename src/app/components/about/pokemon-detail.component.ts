16a9c4695c4590b89f96453764cf271d826770b8
tina <tinarezanezhaad@gmail.com>
2023-11-13 23:43:03 +0100

    New component to show the Pokemon detail.

---
+++ src/app/components/about/pokemon-detail.component.ts
@@ -0,0 +1,34 @@
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
+import {Pokemon} from "../../models/pokemon";
+import {ApiService} from "../../service/api.service";
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './pokemon-detail.component.html',
+  styleUrls: ['./pokemon-detail.component.scss']
+})
+export class PokemonDetailComponent implements OnInit {
+  private pokemonId: number = 0;
+  pokemon: Pokemon = {} as Pokemon;
+
+  constructor(private route: ActivatedRoute,
+              private apiService: ApiService) {
+  }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.pokemonId = params['id'];
+    });
+    this.pokemon.imageUrl = this.apiService.getPokemonImage(this.pokemonId);
+    this.getPokemonData();
+  }
+
+  getPokemonData() {
+
+    this.apiService.getPokemon(this.pokemonId).subscribe(data => {
+      this.pokemon.name = data.name;
+    })
+  }
+
+}

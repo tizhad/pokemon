3cdada03497db99bcc953e95b5e40fa6446e75e3
tina <tinarezanezhaad@gmail.com>
2023-11-14 20:58:20 +0100

    New tests added to the header.component.spec.ts.

--- src/app/components/header/header.component.spec.ts
+++ src/app/components/header/header.component.spec.ts
@@ -1,6 +1,8 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { HeaderComponent } from './header.component';
+import {HeaderComponent} from './header.component';
+import {DebugElement} from "@angular/core";
+import {By} from "@angular/platform-browser";
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -17,5 +19,18 @@ describe('HeaderComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+
+  it('should have a button with Home text.', () => {
+    const btn: DebugElement = fixture.debugElement.nativeElement.querySelector('button');
+    expect(btn).toBeTruthy();
+  });
+
+  it('should call a function when home button clicked. ', () => {
+    const spy = spyOn(component, 'onBackHome');
+    const btn: DebugElement = fixture.debugElement.query(By.css('button'));
+    btn.nativeElement.click();
+    expect(spy).toHaveBeenCalled();
   });
 });
